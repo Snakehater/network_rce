66c82cdf5db0ce45fd1b525e5a32e8b43c85aab3
Snakehater <vigor.turujlija@gmail.com>
2023-01-18 16:49:39 +0100

    Made the slave receive only

--- slave.py
+++ slave.py
@@ -9,15 +9,11 @@ def client_program():
     client_socket = socket.socket()  # instantiate
     client_socket.connect((ip, port))  # connect to the server
 
-    message = input(" -> ")  # take input
-
-    while message.lower().strip() != 'bye':
-        client_socket.send(message.encode())  # send message
-        data = client_socket.recv(1024).decode()  # receive response
-
-        print('Received from server: ' + data)  # show in terminal
-
-        message = input(" -> ")  # again take input
+    while True:
+        data = client_socket.recv(1024).decode()
+        if data == "stop":
+            break
+        print(data)
 
     client_socket.close()  # close the connection
 
